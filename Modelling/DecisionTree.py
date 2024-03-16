6a49fa5bb2d54b13e7eda33205d86adbe6017f9d
NimayKa <akyamin.pgibnu@gmail.com>
2024-03-16 14:46:32 +0800

    Update 1.4.5
    Minor Change
    
    Decision Tree HyperParameter Tuning
    
    Publish by Yamin

--- Modelling/DecisionTree.py
+++ Modelling/DecisionTree.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import os
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 
@@ -11,38 +11,24 @@ file_path = os.path.join(current_directory, 'shopping_behavior_new_updated.csv')
 
 store_df = pd.read_csv('./shopping_behavior_new_updated.csv')
 store_df.dropna(inplace=True)
-print(store_df['Age Group'].unique())
 
 output = store_df['Season']
-features = store_df[['Frequency of Purchases','Age Group','Location','Item Purchased','Category']]
 
-for feature in features:
-    print (len(features[feature].unique()))
+features = store_df[['Frequency of Purchases','Item Purchased', 'Age Group', 'Location', 'Category']]
+output, uniques = pd.factorize(output)
+features = pd.get_dummies(features) 
 
+x_train, x_test, y_train, y_test = train_test_split(features, output, test_size=0.3, random_state=42)
 
-features = pd.get_dummies(features)
-output, uniques = pd.factorize(output) 
+decision_tree_model = DecisionTreeClassifier(criterion= 'entropy',max_depth=12,min_samples_leaf=1,min_samples_split=3)
 
-x_train, x_test, y_train, y_test = train_test_split(
-    features, output, test_size=.2)
+decision_tree_model.fit(x_train, y_train)
 
-# Define the hyperparameters grid
-param_grid = {
-    'criterion': ['gini', 'entropy'],
-    'max_depth': [None, 3, 6, 9, 12, 90],
-    'min_samples_split': [2, 90],
-    'min_samples_leaf': [1, 90]
-}
+y_train_pred = decision_tree_model.predict(x_train)
+y_test_pred = decision_tree_model.predict(x_test)
 
-decision_tree_model = DecisionTreeClassifier()
+train_accuracy = accuracy_score(y_train_pred, y_train)
+test_accuracy = accuracy_score(y_test_pred, y_test)
 
-grid_search = GridSearchCV(decision_tree_model, param_grid, cv=5, scoring='accuracy', n_jobs=-1)
-grid_search.fit(x_train, y_train)
-
-print("Hyperparameter Tuning Results:")
-print()
-results = grid_search.cv_results_
-for mean_score, params in zip(results["mean_test_score"], results["params"]):
-    print("Accuracy:", mean_score)
-    print("Hyperparameters:", params)
-    print()
+print("Training Accuracy:", train_accuracy)
+print("Testing Accuracy:", test_accuracy)
